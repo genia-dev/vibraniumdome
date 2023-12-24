faf873aef78cc4c9658c33f4f5d8e001d044aa4d
Uri Shamay <cmpxchg16@gmail.com>
2023-12-24 15:41:39 +0200

    change the vibraniumdome-app dashboard layout

--- vibraniumdome-app/src/app/page.tsx
+++ vibraniumdome-app/src/app/page.tsx
@@ -5,12 +5,21 @@ import { cn } from "~/app/lib/utils"
 import { buttonVariants } from "~/app/components/ui/button"
 import { UserAuthForm } from "~/app/components/user-auth-form"
 
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "~/server/auth";
+import { redirect } from "next/navigation";
+
 export const metadata: Metadata = {
   title: "Authentication",
   description: "Authentication",
 }
 
-export default function AuthenticationPage() {
+export default async function AuthenticationPage() {
+  const session = await getServerSession(authOptions);
+  if (session) {
+    redirect("/dashboard");
+  }
+
   return (
     <>
       <div className="container relative hidden h-[1000px] flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
@@ -26,19 +35,7 @@ export default function AuthenticationPage() {
         <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
           <div className="absolute inset-0 bg-zinc-900" />
           <div className="relative z-20 flex items-center text-lg font-medium">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              className="mr-2 h-6 w-6"
-            >
-              <path d="M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3" />
-            </svg>
-            Vibranium
+            VibraniumDome
           </div>
           <div className="relative z-20 mt-auto">
             <blockquote className="space-y-2">
