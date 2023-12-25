03d100895e671c444768e407669c067e16d5dcc7
Uri Shamay <cmpxchg16@gmail.com>
2023-12-25 18:32:50 +0200

    remove code duplication for users page placeholder

--- vibraniumdome-app/src/app/users/page.tsx
+++ vibraniumdome-app/src/app/users/page.tsx
@@ -1,5 +1,3 @@
-import { api } from "~/trpc/server";
-import { Button } from "~/app/components/ui/button"
 import {
   Card,
   CardContent,
@@ -8,51 +6,25 @@ import {
   Tabs,
   TabsContent,
 } from "~/app/components/ui/tabs"
-// import { MainNav } from "~/app/dashboard/components/main-nav"
-// import { Search } from "~/app/dashboard/components/search"
-// import { UserNav } from "~/app/dashboard/components/user-nav"
 import { getServerSession } from "next-auth/next";
 import { authOptions } from "~/server/auth";
 import { redirect } from "next/navigation";
-import { DataTable, Policy } from "~/app/policies/components/data-table"
-import { CreatePolicyDialog } from "~/app/policies/components/create-policy-dialog"
 import * as React from "react"
 
-function transformToPolicyArray(jsonData: any[]): Policy[] {
-  const policyArray: Policy[] = [];
-  for (const item of jsonData) {
-    const policy: Policy = {
-      id: item.id,
-      name: item.name,
-      llmAppName: item.llmApp,
-      content: JSON.stringify(item.content),
-      createdAt: item.createdAt
-    };
-    policyArray.push(policy);
-  }
 
-  return policyArray;
-}
-
-export default async function PoliciesTable() {
+export default async function UsersTable() {
   const session = await getServerSession(authOptions);
   if (!session) {
     redirect("/");
   }
-
-  const policies = await api.policy.getAll.query();
-
-  const policiesData = transformToPolicyArray(policies)
   
   return (
     <>
       <div className="hidden flex-col md:flex">
-        
         <div className="flex-1 space-y-4 p-8 pt-6">
           <div className="flex items-center justify-between space-y-2">
             <h2 className="text-2xl font-semibold">Users</h2>
             <div className="flex items-center space-x-2">
-              {/* <CreatePolicyDialog/> */}
             </div>
           </div>
           <Tabs defaultValue="overview" className="space-y-4">
@@ -63,7 +35,6 @@ export default async function PoliciesTable() {
                   <div className="hidden h-full flex-1 flex-col space-y-8 p-8 md:flex">
                     <div className="flex items-center justify-between space-y-2">
                     </div>
-                    {/* <DataTable data={policiesData} /> */}
                   </div>
                   </CardContent>
                 </Card>
