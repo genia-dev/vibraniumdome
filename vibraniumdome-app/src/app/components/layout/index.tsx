e9fbf7cd018b35214c181bbaad80aefe934b4964
Uri Shamay <cmpxchg16@gmail.com>
2024-01-18 14:14:17 +0200

    remove bad import

--- vibraniumdome-app/src/app/components/layout/index.tsx
+++ vibraniumdome-app/src/app/components/layout/index.tsx
@@ -5,7 +5,6 @@ import Header from "~/app/components/layout/header";
 import Sidebar from "~/app/components/layout/sidebar";
 import { SessionProvider, useSession } from "next-auth/react";
 import { ThemeProvider } from "next-themes";
-import { store } from '~/app/store/store';
 
 
 export const Layout = ({ children }: { children: React.ReactNode }) => {
