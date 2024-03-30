91d3825f48383f8f33d1558f1647a693caa19e69
Uri Shamay <cmpxchg16@gmail.com>
2024-03-30 23:08:18 +0300

    make examples to be poetry project to pin the dependencies

--- vibraniumdome-shields/examples/quickstart.py
+++ vibraniumdome-shields/examples/quickstart.py
@@ -1,28 +1,16 @@
 import os
-import openai
+from openai import OpenAI
 
 from vibraniumdome_sdk import VibraniumDome
 
-openai.api_key = os.getenv("OPENAI_API_KEY")
-
 VibraniumDome.init(app_name="set_you_agent_name_here")
 
-def main():
-    response = openai.ChatCompletion.create(
+client = OpenAI(api_key=os.environ["OPENAI_API_KEY"])
+completion = client.chat.completions.create(
     model="gpt-3.5-turbo",
-    messages=[
-        {"role": "system", "content": "You are a helpful assistant."},
-        {"role": "user", "content": "Who won the world series in 2020?"},
-        {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
-        {"role": "user", "content": "Where was it played?"}
-    ],
-    temperature=0,
-    request_timeout=5,
+    messages=[{"role": "user", "content": "Tell me a joke about opentelemetry"}],
     user="user-123456",
-    headers={"x-session-id": "abcd-1234-cdef"},)
+    extra_headers={"x-session-id": "abcd-1234-cdef"},
+)
 
-    print(response)
-
-
-if __name__ == "__main__":
-    main()
+print(completion.choices[0].message.content)
